730987345afb4294a64c024d76b535de3f5019d4
yamenAl <yamen_alsharabi@hotmail.com>
2025-02-03 13:13:24 +0100

    3-2workshop

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import { Liquid } from 'liquidjs';
 
 
 // Vul hier jouw eigen ID in (zie de instructies in de leertaak)
-const personID = 234
+const personID = 191
 
 // Doe een fetch naar een URL op de WHOIS API, ga pas verder als de fetch gelukt is
 const personResponse = await fetch('https://fdnd.directus.app/items/person/' + personID)
